3d52b4c8a7a2d9e088e378b2daed161d8e4de081
Muhammad-Asif-Ansari <muhammadasifansari101@gmail.com>
2025-10-18 04:49:36 +0500

    production deploy successfull

--- api/index.js
+++ api/index.js
@@ -1,7 +1,7 @@
 import express from "express"
 import cors from "cors"
-import route from "./routes/index.js"
-import "./db/index.js"
+import route from "../routes/index.js"
+import "../db/index.js"
 const app = express()
 
 app.use(express.json())
